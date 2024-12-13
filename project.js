233dff241568b289252fd63ef40917229fe9c157
johnny-debug <somaisumnerd458@gmail.com>
2024-12-13 19:34:43 -0300

    atualização no project.js

--- project.js
+++ project.js
@@ -1,12 +1,29 @@
 // 1. Despot some money X
 // 2. Determine number of lines to bet on X
-// 3. Collect a bet amount -
-// 4. Spin the slot machine
+// 3. Collect a bet amount X
+// 4. Spin the slot machine -
 // 5. check if the user won
 // 6. give the user their winnings
 // 7. play again
 
 const prompt = require("prompt-sync")();
+
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+  "A": 2,
+  "B": 4,
+  "C": 6,
+  "D": 8
+}
+
+const SYMBOLS_VALUES = {
+  "A": 5
+  "B": 4
+  "C": 3
+  "D": 2
+}
 
 const deposit = () => {
   while (true) {
@@ -34,5 +51,23 @@ const getNumberOfLines = () => {
       }    
 };
 
-const depositAmount = deposit();
+const getBet = (balance, lines) => {
+  while (true) {
+    const bet = prompt("Enter the bet per line: ");
+    const numberBet = parseFloat(bet);
+    
+    if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+        console.log("Invalid bet, try again.");
+    } else {
+      return numberBet;
+    }  
+  }  
+};
+
+const spin = () => {
+  const symbols = [];
+}
+
+let balance = deposit();
 const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
